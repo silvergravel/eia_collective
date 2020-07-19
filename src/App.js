93937abae51ec241075bb0b7ad5761dbe8d972b3
Abrar Burk <burk.abrar@gmail.com>
2020-07-19 23:35:57 +0530

    :sparkles: implement random email template fetching from jsons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import email_to from './email_content/email_to.json';
+import email_templates from './email_content';
+const { to } = email_to;
 
-function App() {
+const App = () => {
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  const email_to_use = email_templates[getRandomInt(email_templates.length)];
+  let { subject, body } = email_to_use;
+  body = body.join('%0D%0A%0D%0A');
+  let emailString = `mailto:${to}?subject=${subject}&body=${body}`
+
+  useEffect(() => {
+    // const email_to_use = email_templates[getRandomInt(email_templates.length)];
+    // let { subject, body } = email_to_use;
+    // body = body.join('<br>');
+    //
+    // window.open(`mailto:${to}?subject=${subject}&body=${body}`, '_blank');
+    document.getElementById('mail_link').click();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Redirecting you to your email client...</h2>
+      <a id='mail_link' href={emailString} target='_blank' style={{visibility: 'hidden'}}>mail</a>
     </div>
   );
 }
---
+++ src/email_content/index.js
@@ -0,0 +1,11 @@
+import email_1 from './email_1.json';
+import email_2 from './email_2.json';
+import email_3 from './email_3.json';
+
+const email_templates = [
+  email_1,
+  email_2,
+  email_3,
+]
+
+export default email_templates;
